c5b9385db32e3b6c6db7d65d3aa518d410233501
Farkhodoff21 <farkhodoffcodes@gmail.com>
2022-09-01 11:08:20 +1400

    initial commit

---
+++ src/layout/Header.js
@@ -0,0 +1,14 @@
+function Header () {
+    return (
+        <nav>
+        <div className="nav-wrapper header_nav">
+          <a href="#!" classNameName="brand-logo">React</a>
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <li><a href="#!">Movie</a></li>
+          </ul>
+        </div>
+      </nav>
+    )
+}
+
+export default Header;
